c56ada9140e9dd52428a81f8a08744aab4feae8b
eric <ecuiffo@gmail.com>
2013-02-19 01:04:38 -0500

    Added file generator script

---
+++ filegenerator.py
@@ -0,0 +1,18 @@
+import random
+import sys
+
+def createFile( p, n, amount ):
+  f = open('test.in','wb')
+  f.write(p+'\n')
+  f.write(n+'\n')
+  for i in range(int(amount)):
+  	string = str(random.randint(0,int(n)))
+  	string += " "
+  	string += str(random.randint(0,int(n)))
+  	string += '\n'
+  	f.write(string)
+  f.close()
+  
+if __name__ == "__main__":
+  import sys
+  createFile(sys.argv[1], sys.argv[2], sys.argv[3])
