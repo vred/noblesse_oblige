b855b02827e70263188ff5d6e0b2f217cde930a5
eric <ecuiffo@gmail.com>
2013-02-20 05:38:44 -0500

    Almost perfect, just small glitches with larger matrices

--- wtc_btproc.c
+++ wtc_btproc.c
@@ -1,29 +1,36 @@
 #include "wtc.h"
 
-void enqueue( int row, int *queue, int *queueSizeAndIteration )
-{
-	queue[queueSizeAndIteration[0]] = row;
-	queueSizeAndIteration[0]++;
-}
-
-int dequeue( int *queue, int *queueSizeAndIteration )
-{
-	queueSizeAndIteration[0]--;
-	return queue[queueSizeAndIteration[0]];
-}
 
 //Process for child
-
-int childProcess(int processNumber, sem_t* p2c, sem_t* c2p, int numVerts, int numProcs, int* M_prev, int* M_curr, int* queue, int *queueSizeAndIteration)
+int childProcess(int processNumber, sem_t* p2c, sem_t* c2p, int numVerts, int numProcs, int* M_prev, int* M_curr, int* queue, int* iterator, sem_t* queueSem)
 {
-	while ((queueSizeAndIteration[0]>0)||(queueSizeAndIteration[1]<=numVerts))
+	//Outer loop which loops at every iteration
+	while ((*queue<numVerts)||(*iterator<numVerts))
 	{
 		sem_wait(&p2c[processNumber]);
-		int row = dequeue(queue,queueSizeAndIteration);
-		int j;
-		for (j = 0; j < numVerts; j++)
-			if ( (M_prev[(queueSizeAndIteration[1])*(numVerts)+j]==1&&M_prev[(row)*(numVerts)+(queueSizeAndIteration[1])]) || M_prev[(row)*(numVerts)+j]==1 )
-	  		M_curr[row*numVerts+j] = 1;
+		
+		//Inner loop that allows for children to grab from the queue
+		while(*queue<numVerts)
+		{
+			
+			//Wait for any other process to stop accessing this sensitive data
+			sem_wait(queueSem);
+			
+			//Dequeue
+			if(*queue >= numVerts)
+				break;
+			int row = *queue;
+			*queue+=1;
+			
+			//Allow other processes to pass the wait stage
+			sem_post(queueSem);
+
+			//Funtion that computes row
+			int j;
+			for (j = 0; j < numVerts; j++)
+				if ( (M_prev[(*iterator)*(numVerts)+j]==1&&M_prev[(row)*(numVerts)+(*iterator)]) || M_prev[(row)*(numVerts)+j]==1 )
+	  			M_curr[row*numVerts+j] = 1;
+		}
 		sem_post(&c2p[processNumber]);
 	}
   exit(0);
@@ -40,13 +47,14 @@ int wtc_btproc(int numProcs, int numVerts, int** matrix)
   int* M_prev = NULL; //matrixes
   int* M_curr = NULL;
   int* queue = NULL;
-  int* queueSizeAndIteration = NULL;
+  int* iterator = NULL;
+  sem_t* queueSem = NULL;
   
   //Calculate sizes of both
   int size1 = sizeof(sem_t)*numProcs;
   int size2 = sizeof(int)*numVerts*numVerts;
-  int size3 = sizeof(int)*numVerts;
-  int size4 = sizeof(int)*2;
+  int size3 = sizeof(int);
+  int size4 = sizeof(sem_t);
   
   //IDs for shared memory
   char* p2c_name = "/shared_sems"; //semaphore for process to alert children
@@ -54,7 +62,8 @@ int wtc_btproc(int numProcs, int numVerts, int** matrix)
   char* mc_name = "/shared_curr";
   char* mp_name = "/shared_prev";
   char* q_name = "/shared_queue";
-  char* qsai_name = "/shared_queueSizeAndIteration";
+  char* i_name = "/shared_iterator";
+  char* qs_name = "/shared_queueSem";
   
   //Create shared memory file and assign pointers
   int fd1=shm_open(p2c_name, O_RDWR | O_CREAT | O_TRUNC, 0666);
@@ -62,8 +71,8 @@ int wtc_btproc(int numProcs, int numVerts, int** matrix)
   int fd3=shm_open(mc_name, O_RDWR | O_CREAT | O_TRUNC, 0666);
   int fd4=shm_open(mp_name, O_RDWR | O_CREAT | O_TRUNC, 0666);
   int fd5=shm_open(q_name, O_RDWR | O_CREAT | O_TRUNC, 0666);
-  int fd6=shm_open(qsai_name, O_RDWR | O_CREAT | O_TRUNC, 0666);
-
+  int fd6=shm_open(i_name, O_RDWR | O_CREAT | O_TRUNC, 0666);
+	int fd7=shm_open(qs_name, O_RDWR | O_CREAT | O_TRUNC, 0666);
 
   //make the memory file the right size
   ftruncate(fd1,size1*2); 
@@ -71,17 +80,17 @@ int wtc_btproc(int numProcs, int numVerts, int** matrix)
   ftruncate(fd3,size2*2);
   ftruncate(fd4,size2*2);
   ftruncate(fd5,size3*2);
-  ftruncate(fd6,size4*2);
+  ftruncate(fd6,size3*2);
+  ftruncate(fd7,size4*2);
 
-  
   //Map files to memory
   p2c = (sem_t*)mmap(0,size1*2,PROT_READ | PROT_WRITE, MAP_SHARED, fd1, 0);
   c2p = (sem_t*)mmap(0,size1*2,PROT_READ | PROT_WRITE, MAP_SHARED, fd2, 0);
   M_curr = (int*)mmap(0,size2*2,PROT_READ | PROT_WRITE, MAP_SHARED, fd3, 0);
   M_prev = (int*)mmap(0,size2*2,PROT_READ | PROT_WRITE, MAP_SHARED, fd4, 0);
   queue = (int*)mmap(0,size3*2,PROT_READ | PROT_WRITE, MAP_SHARED, fd5, 0);
-  queueSizeAndIteration = (int*)mmap(0,size4*2,PROT_READ | PROT_WRITE, MAP_SHARED, fd6, 0);
-
+  iterator = (int*)mmap(0,size3*2,PROT_READ | PROT_WRITE, MAP_SHARED, fd6, 0);
+  queueSem = (sem_t*)mmap(0,size4*2,PROT_READ | PROT_WRITE, MAP_SHARED, fd7, 0);
 	
   //Initialize all semaphores
   int k;
@@ -94,37 +103,41 @@ int wtc_btproc(int numProcs, int numVerts, int** matrix)
       }
   }
   
-  //Initialize pass iterator and queueSize
-  queueSizeAndIteration[0] = 0;
-  queueSizeAndIteration[1] = 0;
+  if(sem_init(queueSem, 1, 0)==-1)
+ 	{
+		fprintf(stderr,"sem_init failed\n");
+		exit(1);
+  }
+  
+  //Initialize queue and iterator
+  *queue = 0;
+  *iterator = 0;
 
-  //Copy original matrix to shared memory, and initialize queue
+  //Copy original matrix to shared memory
   int i, j;
   for(i=0; i<numVerts; i++)
     for(j=0; j<numVerts; j++)
       M_prev[i*numVerts+j]=matrix[i][j];
 	
   //Create the desired number of processes
+  int pid;
   for(k=0; k<numProcs; k++){
-	int pid=fork();
+	pid=fork();
 		if(!pid)
-		{
-			printf("Why aren't my children here???");
-			childProcess(k,p2c,c2p,numVerts,numProcs,M_prev,M_curr,queue,queueSizeAndIteration);
-		}	
+			childProcess(k,p2c,c2p,numVerts,numProcs,M_prev,M_curr,queue, iterator, queueSem);
 	}
 	
   int y,z;
   //Counter for each iteration
-  for(queueSizeAndIteration[1]=0; queueSizeAndIteration[1]<numVerts; queueSizeAndIteration[1]++) 
+  for(; *iterator<numVerts; *iterator+=1) 
   {	
+		//reset queue
+  	*queue = 0;
+
   	//Make the processes stop waiting
+  	sem_post(queueSem);
   	for(k=0; k<numProcs; k++)
 			sem_post(&p2c[k]);
-  	
-  	//Pushes all tasks into queue
-  	for(i=0; i<numVerts; i++)
-  		enqueue(i,queue,queueSizeAndIteration);
   		
     //Waits for all children to finish the processes
     for(k=0; k<numProcs; k++)
@@ -160,7 +173,7 @@ int wtc_btproc(int numProcs, int numVerts, int** matrix)
   close(fd5);
   shm_unlink(q_name);
   close(fd6);
-  shm_unlink(qsai_name);
+  shm_unlink(i_name);
   
   gettimeofday(&endt, NULL);
   int elapsedTime;
