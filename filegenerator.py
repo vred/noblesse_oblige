128bd2583e92d62ab8195f4582a12da696e785a7
eric <ecuiffo@gmail.com>
2013-02-19 22:00:15 -0500

    Oh that's why it wasn't working..

--- filegenerator.py
+++ filegenerator.py
@@ -5,18 +5,26 @@ def createFile( path, p, n ):
   f = open(path,'wb')
   f.write(p+'\n')
   f.write(n+'\n')
-  j = 0
-  for i in range(int(n)):
-  	if j%int(n) != (int(n))/2:
-  		string = str(j%int(n)+1)
-  		string += " "
-  		if (j+1)%int(n) == 0:
-  			string += str(1)
+  for i in range(1,int(n)+1):
+  	if i >= (int(n))/2:
+  		j=i+1
+  		k=i+2
+  	else:
+  		j=i
+  		if(i+1 >= (int(n))/2):
+  			k=i+2
   		else:
-  			string += str(j%int(n)+2)
-  		string += '\n'
-  		f.write(string)
-  	j=j+1
+  			k=i+1
+  	string = str(j)
+  	string += " "
+  	if (j)%int(n) == 0:
+  		string += str(1)
+  	else:
+  		string += str(k)
+  	string += '\n'
+  	f.write(string)
+  	if (j)%int(n) == 0:
+  		break
   f.close()
   
 if __name__ == "__main__":
