24172ec2293cc3fa595fe40a6148599841afeeb9
Eric Cuiffo <cuiffo@flyweight.rutgers.edu>
2013-02-20 19:20:55 -0500

    SUP

--- reader_template.py
+++ reader_template.py
@@ -4,10 +4,16 @@ This code will automatically write a comma delimited stuff for stuff.
 """
 import sys
 import os
-import argparse
-import DebianMetaReader
 
-if(args.String!=None):
-    newdata=os.system("apt-cache show "+args.String+">.YouCantSeeMe.txt")
 
-fileName = '.YouCantSeeMe.txt'
+def viewFile():
+  pArray = [2,4,8,16,32,64]
+  nArray = [2,4,6,8,10,20,30,40,50,60,70,80,90,100,200,300,400,500,600,700,800,900,1000,2000,3000,4000,5000,6000,7000,8000,9000,10000]
+  for p in pArray:
+    for n in nArray:
+      newdata=os.system("./wtc 1 inputFiles/inputX"+str(p)+"X"+str(n))
+      print newdata
+
+if __name__ == "__main__":
+  import sys
+  viewFile()
