1219fb652b72b4a697af3046974ec3318450961d
Val A. Red <vred@scarletmail.rutgers.edu>
2013-02-20 19:02:27 -0500

    Stuff got added.

---
+++ reader_template.py
@@ -0,0 +1,13 @@
+# Autograph
+"""
+This code will automatically write a comma delimited stuff for stuff.   
+"""
+import sys
+import os
+import argparse
+import DebianMetaReader
+
+if(args.String!=None):
+    newdata=os.system("apt-cache show "+args.String+">.YouCantSeeMe.txt")
+
+fileName = '.YouCantSeeMe.txt'
