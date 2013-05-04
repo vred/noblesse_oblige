c9e629b0c7c0b676ef08814a40bb9eb2b2fc99d5
Nick Palumbo <shmozer@gmail.com>
2013-05-04 16:58:39 -0400

    FUCK THE WORLD EVERYTHING IS BREAKING AND CHANGING

--- A03/clientSNFS/clientSNFS.c
+++ A03/clientSNFS/clientSNFS.c
@@ -14,13 +14,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include "../SNFS.h"
+
 //contains the IP address where the server sits
-char[16] ip;
+char ip[17];
 //contains the port where the server is listening
 int port;
+//Contains the mount path
+char* mount_path;
 
 //send request to server through socket and get feedback
-static int sendRequestToServer(const char* path, char* buf, int size){
+char* sendRequestToServer(char* request_data, char* buf, int size){
 
  		int sock, bytes_recieved;  
         struct hostent *host;
@@ -30,7 +34,7 @@ static int sendRequestToServer(const char* path, char* buf, int size){
 
         if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
             perror("Socket error");
-            return -1;
+            return NULL;
         }
 
         server_addr.sin_family = AF_INET;     
@@ -42,64 +46,129 @@ static int sendRequestToServer(const char* path, char* buf, int size){
                     sizeof(struct sockaddr)) == -1) 
         {
             perror("Connect error");
-            return -1;
+            return NULL;
         }
         
-        //Only read is implemented here, so just send the path to server
-		send(sock,path,strlen(path), 0);
+        //Send our struct WTF is its size?
+		send(sock,request_data,strlen(request_data), 0);
 				
 		//Receive the content of file
 		bytes_recieved=recv(sock,buf,size,0);
         
         buf[bytes_recieved] = '\0';
-
+					
 		close(sock);
 
-		return 0;
+		return buf;
 }
 
-static int SNFS_getattr(const char *path, struct stat *stbuf)
+//Getattr implementation
+static int SNFS_getattr(const char *path, struct stat *stbuf){
 	
+	int retstat = 0;
+	char* request = malloc(1000);
+	char* response = malloc(11111);
+	strcpy(request,"getattr, ");
+	strcat(request, path);
+	//request all the things in the directory from the server
+	sendRequestToServer(request, response, 1024);
+	memcpy(stbuf,response,sizeof(response));
+	return retstat;
 }
 
+
+
+//Readdir implementation
 static int SNFS_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 			 off_t offset, struct fuse_file_info *fi){
+	int retstat = 0;
+	char* request = malloc(1000);
+	char* response = malloc(11111);
+	strcpy(request,"readdir, ");
+	strcat(request, path);
+	//request all the things in the directory from the server
+	sendRequestToServer(request, response, 1024);
 	
-				 
+	//call filler on each of those things
+	char* token = strtok(response, " ,");
+	while (token != NULL) {
+		filler(buf,token,NULL,0);
+		token = strtok(NULL, " ,");
+	}
+	
+	return retstat;
 }
 
-static int SNFS_open(const char *path, struct fuse_file_info *fi){
+/*static int SNFS_open(const char *path, struct fuse_file_info *fi){
 
 }
 
 static int SNFS_read(const char *path, char *buf, size_t size, off_t offset,
 		      struct fuse_file_info *fi){
 				  
-}
+}*/
 
 static struct fuse_operations SNFS_oper = {
-	.create		= SNFS_create,
-	.open		= SNFS_open,
-	.write		= SNFS_write,
-	.close		= SNFS_close,
-	.truncate	= SNFS_truncate,
-	.opendir	= SNFS_opendir,
+	//.create		= SNFS_create,
+	//.open		= SNFS_open,
+	//.write		= SNFS_write,
+	//.close		= SNFS_close,
+	//.truncate	= SNFS_truncate,
+	//.opendir	= SNFS_opendir,
 	.readdir	= SNFS_readdir,
-	.releasedir	= SNFS_releasedir,
-	.mkdir		= SNFS_mkdir,
+	//.releasedir	= SNFS_releasedir,
+	//.mkdir		= SNFS_mkdir,
 	.getattr	= SNFS_getattr,
 };
 
 int main(int argc, char *argv[])
 {
-	if(argc != 4){
+	if(argc != 7){
 		fprintf(stderr,"Invalid number of arguments!\n");
 		return -1;
 	}
 	
-	port = atoi(argv[1]);
-	ip = argv[2];
+	//Check the flags
+	if(!strcmp(argv[1],"-port")){
+		port = atoi(argv[2]);
+	}
+	else if(!strcmp(argv[1],"-address")){
+		strcpy(ip,argv[2]);
+	}
+	else if(!strcmp(argv[1],"-mount")){
+		mount_path = strdup(argv[2]);
+	}
+	else{
+		perror("Incorrect arguments!");	
+	}
+	if(!strcmp(argv[3],"-port")){
+		port = atoi(argv[4]);
+	}
+	else if(!strcmp(argv[3],"-address")){
+		strcpy(ip,argv[4]);
+	}
+	else if(!strcmp(argv[3],"-mount")){
+		mount_path = strdup(argv[4]);
+	}
+	else{
+		perror("Incorrect arguments!");	
+	}
+	if(!strcmp(argv[5],"-port")){
+		port = atoi(argv[6]);
+	}
+	else if(!strcmp(argv[5],"-address")){
+		strcpy(ip,argv[6]);
+	}
+	else if(!strcmp(argv[5],"-mount")){
+		mount_path = strdup(argv[6]);
+	}
+	else{
+		perror("Incorrect arguments!");	
+	}
 	
-	
-	return fuse_main(argc, argv, &hello_oper, NULL);
+	char** fuse_pass_argument = malloc(2*sizeof(char*));
+	fuse_pass_argument[0] = strdup(argv[0]);
+	fuse_pass_argument[1] = strdup(mount_path);
+		
+	return fuse_main(2, fuse_pass_argument, &SNFS_oper, NULL);
 }
--- A03/serverSNFS/serverSNFS.c
+++ A03/serverSNFS/serverSNFS.c
@@ -1,4 +1,5 @@
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -7,17 +8,66 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <pthread.h>
+#include <dirent.h>
+#include "../SNFS.h"
 
-static int remote_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-			 off_t offset, struct fuse_file_info *fi){
-					 
+
+//contains the port where the server is listening
+int port;
+
+char* request_handler(char* request){
+
+
+	char* tokenreq = strtok(request," ,");
+	char* firstarg = tokenreq;
+
+	if( !strcmp(firstarg,"readdir") )
+	{
+		tokenreq = strtok(NULL, " ,");
+		return remote_readdir(tokenreq);
+	} else if( !strcmp(firstarg,"getattr")){
+		tokenreq = strtok(NULL, " ,");
+		return remote_getattr(tokenreq);
+	}
+	//switch on first argument of request which is system call name
+	
+}
+
+char* remote_getattr(const char *path){
+	struct stat* stat_buf = (struct stat*)malloc(sizeof(struct stat));
+	lstat(path,stat_buf);
+	char* status = malloc(sizeof(stat_buf));
+	memcpy(status, stat_buf, sizeof(struct stat));
+	return status;
+}
+
+char* remote_readdir(const char *path){
+	DIR *dir;
+	char* entry_list = (char*)malloc(1);
+	struct dirent *ent;
+	if ((dir = opendir (path)) != NULL) {
+	  /* print all the files and directories within directory */
+	  while ((ent = readdir (dir)) != NULL) {
+	    entry_list = (char*)realloc(entry_list,sizeof(remote_readdir)+strlen(ent->d_name)+2);
+	    entry_list = strcat(entry_list,", ");
+	    entry_list = strcat(entry_list,ent->d_name);
+	  }
+	  closedir (dir);
+	} else {
+	  /* could not open directory */
+	  perror ("");
+	  return NULL;
+	}
+	return entry_list;			 
 }
 
 
 int main()
 {
+	port=5000;
         int sock, connected, bytes_recieved , true = 1;  
-        char recv_data[1024];       
+        char rec_data[1024];    
 
         struct sockaddr_in server_addr,client_addr;    
         int sin_size;
@@ -33,7 +83,7 @@ int main()
         }
         
         server_addr.sin_family = AF_INET;         
-        server_addr.sin_port = htons(5000);     
+        server_addr.sin_port = htons(port);     
         server_addr.sin_addr.s_addr = INADDR_ANY; 
         bzero(&(server_addr.sin_zero),8); 
 
@@ -48,43 +98,45 @@ int main()
             exit(1);
         }
 		
-		printf("\nTCPServer Waiting for client on port 5000");
+		printf("\nTCPServer Waiting for client on port %d\n",port);
         fflush(stdout);
 
 
         while(1)
         {  
+			printf("waiting\n");
 
             sin_size = sizeof(struct sockaddr_in);
 
             connected = accept(sock, (struct sockaddr *)&client_addr,&sin_size);
 
-            printf("\n I got a connection from (%s , %d)",
+            printf("I got a connection from (%s , %d)\n",
                    inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
 
 			//In this exmaple, server is only single-threaded
             {
-
-              bytes_recieved = recv(connected,recv_data,1024,0);
-
-              recv_data[bytes_recieved] = '\0';
-
-			  printf("\nClient wants to read file = %s " , recv_data);
+				
+              bytes_recieved = recv(connected,rec_data,1024,0);
+				
+             rec_data[bytes_recieved]='\0';
+				              
+              printf("SUP? PATH = %s\n",rec_data);
+              
+               fflush(stdout);
+              
+              char* response = request_handler(rec_data);
+              
+              printf("Got my response, it's %d big and its %s\n",strlen(response),response);
+              
+              send(connected,response,strlen(response),0);
+			
+				printf("And I sent it\n");
+			 // printf("\nClient wants to read file = %s " , recv_data);
    	
-			  fflush(stdout);
+			 
 
 			  //Assume only file01 and file02 are existing now
-			  static const char* file01_content = "You can see file01\n";
-
-			  static const char* file02_content = "You can see file02\n";
-
-              if (strcmp(recv_data , "/file01") == 0)
-              {
-                send(connected,file01_content,strlen(file01_content),0);
-              }
-			  else if( strcmp(recv_data , "/file02") ==0 ){
-                send(connected,file02_content,strlen(file02_content),0);
-			  } 
+			  
                
               close(connected);
 
